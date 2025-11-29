67ca24f9a2349466704b00ea10f088d669c4b229
camoalex30 <alex.camouflage.robinson@gmail.com>
2025-11-29 21:19:09 +0000

    adding blink led code

---
+++ blink_dat_led.ino
@@ -0,0 +1,10 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT); // Set the built-in LED pin as an output
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH); // Turn the LED on
+  delay(100); // Wait for one second
+  digitalWrite(LED_BUILTIN, LOW); // Turn the LED off
+  delay(1000); // Wait for one second
+}
